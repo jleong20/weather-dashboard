1956856cd6f95d35f89ef96868193058bbb4a0ca
Jerry Leong <jerry.leong20@gmail.com>
2020-09-30 23:06:14 -0700

    add temperature of city

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var APIKey = "e6ed66e2161fbded007c85d013a1bf39";
+var APIKey = "c241e584f19cb878d9abf3b10c21674b";
 var cityName = "san francisco"
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&appid=" +APIKey;
-
+var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" +cityName+ "&units=imperial&appid=" +APIKey;
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    $("#city-name").text(response.name);
-    $("#temp").append(response.main.temp);
-    $("#humid").append(response.main.humidity);
-    $("#wind").append(response.wind.speed);
-    
-  });
+    $("#city-name").append(response.city.name);
+    $("#temp").append(response.list[0].main.feels_like + " °F");
+    $("#humid").append(response.list[0].main.humidity);
+    $("#wind").append(response.list[0].wind.speed);
+    var lat = response.city.coord.lat;
+    var lon = response.city.coord.lon;
+});
 
 
