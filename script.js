480f8ccdc926b9f156c43afcdc8b779e672d2307
Jerry Leong <jerry.leong20@gmail.com>
2020-09-30 22:03:21 -0700

    add city to dashboard

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 var APIKey = "e6ed66e2161fbded007c85d013a1bf39";
+var cityName = "san francisco"
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&appid=" +APIKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    $("#city-name").text(response.name);
+    $("#temp").append(response.main.temp);
+    $("#humid").append(response.main.humidity);
+    $("#wind").append(response.wind.speed);
+    
+  });
+
+
